e1985b6cbeff5ba32bf4b373da08519ad821fc0f
bahma <bahma@DESKTOP-SQIM9V0>
2026-03-07 11:07:10 +0200

    Add project files.

---
+++ MaxCinema/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MaxCinema.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

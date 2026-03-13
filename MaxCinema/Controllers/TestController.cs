cb6f21c973ebd0db8580ab7e7d7150dc89e20b1d
Максим Петков <mp92974299@edu.mon.bg>
2026-03-13 19:50:06 +0200

    Нови промени 2

---
+++ MaxCinema/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MaxCinema.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("✅ Test controller работи!");
+        }
+    }
+}
